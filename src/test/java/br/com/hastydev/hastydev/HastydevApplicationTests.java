b380ad1809fe77a6809ddd94b587d8b7ab86fcb9
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-03-10 22:15:39 -0300

    Setup Spring Project

---
+++ src/test/java/br/com/hastydev/hastydev/HastydevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hastydev.hastydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HastydevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
